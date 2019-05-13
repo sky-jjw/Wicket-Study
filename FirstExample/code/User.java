ea6e905c8cb34a5930a73dfb736ab68477ab40a0
剑客 <sjt1786843257@163.com>
2019-05-13 13:25:06 +0800

    补充文件

---
+++ FirstExample/code/User.java
@@ -0,0 +1,89 @@
+package TestWicket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String account;
+    private String name;
+    private String password;
+    private boolean female;
+    private double salary;
+    private String job;
+    private int workYear;
+    private boolean married;
+    private List favorites = new ArrayList();
+    
+    public User() {}
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isFemale() {
+        return female;
+    }
+
+    public void setFemale(boolean female) {
+        this.female = female;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getWorkYear() {
+        return workYear;
+    }
+
+    public void setWorkYear(int workYear) {
+        this.workYear = workYear;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    public List getFavorites() {
+        return favorites;
+    }
+
+    public void setFavorites(List favorites) {
+        this.favorites = favorites;
+    }
+}
